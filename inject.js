d428aca917f210583450a3e3d86b1069cfcd9949
Dara Javaherian <dara@adverts.ie>
2016-02-24 23:47:17 +0000

    Make look like the original youtube

--- inject.js
+++ inject.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   $("#search-btn")
-    .unwrap() // Remove the form submitting the query
+    // remove the form submitting the search query
+    .unwrap()
+    // and add a new element with the same id and classes to keep style
+    .wrap("<span id='masthead-search' class='search-form consolidated-form'></span>")
     .attr("onclick", "")
     .click(runThisLittleBeastInstead);
 });
