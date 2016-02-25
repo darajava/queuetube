f7d7ba9cf36c2eff98dc294247883adc4f6519e4
Dara Javaherian <dara@adverts.ie>
2016-02-25 02:00:31 +0000

    reliably swap out old and new search bar

--- inject.js
+++ inject.js
@@ -1,21 +1,39 @@
+var $originalSearch;
+var $mySearch;
+
 $(document).ready(function(){
-  // If this is a video
-  if(window.location.href.indexOf("www.youtube.com/watch?v=") != -1) {
-    $("#search-btn")
+  $originalSearch = $("#masthead-search").css("border", "1px solid green").clone(true, true);
+  $mySearch = $("#masthead-search").clone(true, true);
+
+  $mySearch = $mySearch.find("#search-btn")
       // remove the form submitting the search query
       .unwrap()
       // and add a new element with the same id and classes to keep style
       .wrap("<span id='masthead-search' class='search-form consolidated-form'></span>")
       .attr("onclick", "")
       .css("border", "1px solid red")
-      .click(runThisLittleBeastInstead);
-  }
+      .click(runThisLittleBeastInstead).parent().append($("#masthead-search-terms"));
+  logElements();
+
+  changeSearchBar();
 });
 
-$(window).on('hashchange', function(e){
+
+function changeSearchBar() {
+  // If this is a video
+  if(window.location.href.indexOf("www.youtube.com/watch?v=") != -1) {
+    //alert('vid');
+    $("#yt-masthead-content").empty().append($mySearch);
+  } else {
+    $("#yt-masthead-content").empty().append($originalSearch);
+  }
 }
 
 function runThisLittleBeastInstead() {
   alert();
 }
 
+function logElements(){
+  console.dir($originalSearch);
+  console.dir($mySearch);
+}
