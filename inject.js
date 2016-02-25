36bbdecc37f3b210c5578a5de56b7c39c91e4695
Dara Javaherian <dara@adverts.ie>
2016-02-25 00:11:09 +0000

    Only load background search on videos... kiiiinda only works on hard refresh as youtube dont fully refresh the page

--- inject.js
+++ inject.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-  $("#search-btn")
-    // remove the form submitting the search query
-    .unwrap()
-    // and add a new element with the same id and classes to keep style
-    .wrap("<span id='masthead-search' class='search-form consolidated-form'></span>")
-    .attr("onclick", "")
-    .click(runThisLittleBeastInstead);
+  // If this is a video
+  if(window.location.href.indexOf("www.youtube.com/watch?v=") != -1) {
+    $("#search-btn")
+      // remove the form submitting the search query
+      .unwrap()
+      // and add a new element with the same id and classes to keep style
+      .wrap("<span id='masthead-search' class='search-form consolidated-form'></span>")
+      .attr("onclick", "")
+      .css("border", "1px solid red")
+      .click(runThisLittleBeastInstead);
+  }
 });
 
+$(window).on('hashchange', function(e){
+}
 
 function runThisLittleBeastInstead() {
   alert();
